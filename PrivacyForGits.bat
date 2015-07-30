8ef6e5a142dfa765a4f3eb82e07b767499026f25
Ugion <samuel@tfwno.gf>
2015-07-30 14:42:19 +0200

    Added remaining privacy settings.
    This may not be feature complete, send in suggestions under Issues.

--- PrivacyForGits.bat
+++ PrivacyForGits.bat
@@ -11,6 +11,12 @@ if "%start% == "n" exit
 :privacystart
 echo Turning off Advertising ID Sharing
 reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Windows\CurrentVersion\AdvertisingInfo /t REG_DWORD /v Enabled /d 0 /f
+echo Disabling Microsoft Keylogger/Sending info on how you write
+reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Input\TIPC /t REG_DWORD /v Enabled /d 0 /f
+echo Setting Telemetry to Basic
+reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection\ /t REG_DWORD /v AllowTelemetry /d 1 /f
+echo I have not figured out how to turn off Wi-Fi Sense, go to Wi-Fi settings/Manage Wi-Fi Settings to turn it off.
+
 echo Done!
 pause
 exit
