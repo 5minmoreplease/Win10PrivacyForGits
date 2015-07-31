fed273cd3ad96f5975d5279e0cb3c03bafc6f9c5
Ugion <samuel@tfwno.gf>
2015-07-31 11:18:25 +0200

    Disabling DiagTrack and blocking Telemetry in hosts.

--- PrivacyForGits.bat
+++ PrivacyForGits.bat
@@ -18,6 +18,31 @@ echo Disabling Microsoft Keylogger/Sending info on how you write
 reg add HKEY_CURRENT_USER\SOFTWARE\Microsoft\Input\TIPC /t REG_DWORD /v Enabled /d 0 /f
 echo Setting Telemetry to Basic
 reg add HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\DataCollection\ /t REG_DWORD /v AllowTelemetry /d 1 /f
+echo Blocking Telemetry in hosts file
+@echo 127.0.0.1 vortex.data.microsoft.com >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 vortex-win.data.microsoft.com >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 telecommand.telemetry.microsoft.com >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 telecommand.telemetry.microsoft.com.nsatc.net >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 oca.telemetry.microsoft.com >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 oca.telemetry.microsoft.com.nsatc.net >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 sqm.telemetry.microsoft.com >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 sqm.telemetry.microsoft.com.nsatc.net >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 watson.telemetry.microsoft.com >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 watson.telemetry.microsoft.com.nsatc.net >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 redir.metaservices.microsoft.com >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 choice.microsoft.com >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 choice.microsoft.com.nsatc.net >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 df.telemetry.microsoft.com  >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 reports.wes.df.telemetry.microsoft.com >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 services.wes.df.telemetry.microsoft.com >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 sqm.df.telemetry.microsoft.com >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 telemetry.microsoft.com >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 watson.ppe.telemetry.microsoft.com >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 telemetry.appex.bing.net >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 telemetry.urs.microsoft.com >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 telemetry.appex.bing.net:443 >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 settings-sandbox.data.microsoft.com >> C:\Windows\System32\drivers\etc\hosts
+@echo 127.0.0.1 vortex-sandbox.data.microsoft.com >> C:\Windows\System32\drivers\etc\hosts
 echo Turning off Wi-Fi Sense
 reg add HKLM\SOFTWARE\Microsoft\WcmSvc\wifinetworkmanager\features\%SID% /v FeatureStates /f /t REG_DWORD /d 828
 echo Done!
